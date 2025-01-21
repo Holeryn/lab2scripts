3ffec1d4b89f3903022b62f776f5cc9f7cba9ebc
Holeryn <Holeryn@protonmail.com>
2025-01-21 20:23:00 +0100

    Add files via upload

--- Tiamat.py
+++ Tiamat.py
@@ -222,3 +222,27 @@ def consistenza_statistica(X,Y,incX,incY):
         return False,a
     else:
         return True,a
+
+def Error_from_two_lines(slope1,intercept1,cov12,slope2,intercept2,cov21):
+    """Calculare the error from the intersection of two straight line
+    of slope: slope1,slope2 , intercept: intercept1,intercept2 and covaratiantons:
+    cov12,cov21"""
+    B1 = -0.28
+    B2 = np.pi/4
+    A1 = -4.33
+    A2 = 1
+    SA1 = 3.26*10**(-0)
+    SA2 = 0
+    SB1 = 0.044
+    SB2 = 0
+    COVA1B1 = -8.533*(10**(-5))
+    COVA2B2 = -4.079*(10**(-3))
+
+    DvtA1 = -(B1-B2)/((A1-A2)**2) 
+    DvtA2 = -DvtA1
+
+    DvtB1 = -1/(A1-A2)
+    DvtB2 = -DvtB1
+
+    COSO1 = (DvtA1**2)*(SA1**2) + (DvtB1**2)*(SB1**2) + (DvtB2**2)*(SB2**2) + (DvtA2**2)*(SA2**2) + 2*DvtA1*DvtB1*COVA1B1 + 2*DvtA2*DvtB2*COVA2B2
+    return COSO1 
